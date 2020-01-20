c4e75dd58beab26dd3c5f3f13c77a52b3cbcce24
emily <mle4d@me.com>
2020-01-19 22:31:14 -0800

    get video to play

--- index.js
+++ index.js
@@ -5,3 +5,17 @@ const progressBar = player.querySelector('.progress__filled');
 const toggle = player.querySelector('.toggle');
 const skipButtons = player.querySelectorAll('[data-skip]');
 const ranges = player.querySelectorAll('.player__slider');
+
+function togglePlay() {
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
+}
+function updateButton() {
+  const icon = this.paused ? '►' : '❚ ❚';
+  console.log(icon);
+  toggle.textContent = icon;
+}
+
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+toggle.addEventListener('click', togglePlay);
